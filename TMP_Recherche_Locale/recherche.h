cb15a9f3f0cd60e9aa04ddf0c0072369ddbaf660
Antoine Francois <a.francois962@laposte.net>
2016-11-11 17:27:59 +0100

    pareil mais les .h sont propres maintenant

--- TMP_Recherche_Locale/recherche.h
+++ TMP_Recherche_Locale/recherche.h
@@ -6,7 +6,4 @@ bool find(queue<Partition> part_queue, const Partition& part);
 
 int recherche_naive(Partition part, const int &n, const int &comparison_type);
 int methode_tabou(Partition part, const int &l, const int &n, const int& comparison_type);
-//avec la comparaison plus fine des rails
-int methode_tabou2(Partition part, const int &l, const int &n);
-bool compare(Partition bestpart, Partition part1, int indicemax);
 int methode_recuit(Partition part, float alpha, const int &comparison_type);
